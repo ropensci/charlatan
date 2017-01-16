eb42ff7eb09b008cf54eba6f39102107643163fe
Scott Chamberlain <myrmecocystus@gmail.com>
2017-01-16 12:52:34 -0800

    added remainder of locale sets for the job provider

--- R/job.R
+++ R/job.R
@@ -2,15 +2,21 @@
 #'
 #' @export
 #' @template params
-#' @param locale (character) the locale to use. only options right now are
-#' en_US, fr_FR, hr_FR, more coming. default: en_US
+#' @param locale (character) the locale to use. options: en_US (default),
+#' fr_FR, fr_CH, hr_FR, fa_IR, pl_PL, ru_RU, uk_UA, zh_TW.
 #' @examples
 #' ch_job()
 #' ch_job(10)
 #' ch_job(500)
 #'
 #' ch_job(locale = "fr_FR", n = 10)
+#' ch_job(locale = "fr_CH", n = 10)
 #' ch_job(locale = "hr_HR", n = 10)
+#' ch_job(locale = "fa_IR", n = 10)
+#' ch_job(locale = "pl_PL", n = 10)
+#' ch_job(locale = "ru_RU", n = 10)
+#' ch_job(locale = "uk_UA", n = 10)
+#' ch_job(locale = "zh_TW", n = 10)
 ch_job <- function(n = 1, locale = NULL) {
   assert(n, c('integer', 'numeric'))
   if (n == 1) {
