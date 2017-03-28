80e99557b46c301963d4a153e449f3ae7c1c2a1c
Scott Chamberlain <myrmecocystus@gmail.com>
2017-03-27 22:20:47 -0700

    fix #25 - change ch_sequence to ch_gene_sequence

--- R/sequences.R
+++ R/sequences.R
@@ -4,13 +4,13 @@
 #' @template params
 #' @param length (integer) length of sequence to create
 #' @examples
-#' ch_sequence()
-#' ch_sequence(10)
-#' ch_sequence(100)
+#' ch_gene_sequence()
+#' ch_gene_sequence(10)
+#' ch_gene_sequence(100)
 #'
-#' ch_sequence(length = 500)
-#' ch_sequence(10, length = 500)
-ch_sequence <- function(n = 1, length = 30) {
+#' ch_gene_sequence(length = 500)
+#' ch_gene_sequence(10, length = 500)
+ch_gene_sequence <- function(n = 1, length = 30) {
   assert(n, c('integer', 'numeric'))
   if (n == 1) {
     SequenceProvider$new()$render(length)
