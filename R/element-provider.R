f6f0539ababd8d9e7778433f8ed04d69b94bb7d8
Scott Chamberlain <myrmecocystus@gmail.com>
2017-12-02 09:44:37 -0800

    added elements provider fix #55

---
+++ R/element-provider.R
@@ -0,0 +1,36 @@
+el_symbols <- c("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og")
+el_elements <- c("Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Oganesson")
+elements <- data.frame(symbol = el_symbols, element = el_elements, stringsAsFactors = FALSE)
+
+#' ElementProvider
+#'
+#' @export
+#' @keywords internal
+#' @details
+#' \strong{Methods}
+#'   \describe{
+#'     \item{\code{render()}}{
+#'       Get an element
+#'     }
+#'   }
+#' @format NULL
+#' @usage NULL
+#' @details Data from Wikipedia at 
+#' <https://en.wikipedia.org/wiki/Chemical_element>
+#' @examples
+#' z <- ElementProvider$new()
+#' z$symbol()
+#' z$element()
+ElementProvider <- R6::R6Class(
+  inherit = BaseProvider,
+  'ElementProvider',
+  public = list(
+    symbol = function() {
+      super$random_element(elements$symbol)
+    },
+
+    element = function() {
+      super$random_element(elements$element)
+    }
+  )
+)
---
+++ R/elements.R
@@ -0,0 +1,34 @@
+#' Get elements
+#'
+#' @export
+#' @name elements
+#' @template params
+#' @examples
+#' ch_element_symbol()
+#' ch_element_symbol(10)
+#' ch_element_symbol(50)
+#'
+#' ch_element_element()
+#' ch_element_element(10)
+#' ch_element_element(50)
+ch_element_symbol <- function(n = 1) {
+  assert(n, c('integer', 'numeric'))
+  if (n == 1) {
+    ElementProvider$new()$symbol()
+  } else {
+    x <- ElementProvider$new()
+    replicate(n, x$symbol())
+  }
+}
+
+#' @export
+#' @rdname elements
+ch_element_element <- function(n = 1) {
+  assert(n, c('integer', 'numeric'))
+  if (n == 1) {
+    ElementProvider$new()$element()
+  } else {
+    x <- ElementProvider$new()
+    replicate(n, x$element())
+  }
+}
