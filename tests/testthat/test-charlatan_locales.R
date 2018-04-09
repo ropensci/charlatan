1d9710fc2dd5a43166f98cc3f0ec502136d218a8
Scott Chamberlain <myrmecocystus@gmail.com>
2018-04-09 08:51:01 -0700

    fix a test

--- tests/testthat/test-charlatan_locales.R
+++ tests/testthat/test-charlatan_locales.R
@@ -2,11 +2,10 @@ context("charlatan_locales")
 
 test_that("charlatan_locales works", {
   expect_is(charlatan_locales, "function")
-  expect_is(charlatan_locales(), "character")
-  expect_gt(length(charlatan_locales()), 10)
+  expect_is(charlatan_locales(), "data.frame")
+  expect_gt(NROW(charlatan_locales()), 40)
 })
 
-# same for available_locales
 test_that("available_locales works", {
   expect_is(available_locales, "character")
   expect_gt(length(available_locales), 10)
