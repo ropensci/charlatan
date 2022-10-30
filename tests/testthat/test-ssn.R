8009902a0b0570102fb4b8827321d9abf90b1e50
Roel Hogervorst <RMHogervorst@users.noreply.github.com>
2022-10-30 08:42:03 +0100

    Apply suggestions from code review

--- tests/testthat/test-ssn.R
+++ tests/testthat/test-ssn.R
@@ -8,7 +8,7 @@ test_that("SSNProvider works", {
 
   expect_is(aa$render, "function")
   expect_is(aa$render(), "character")
-  expect_match(aa$render(), "\\d\\d\\d-\\d\\d-\\d\\d\\d\\d") # :(
+  expect_match(aa$render(), "\\d\\d\\d-\\d\\d-\\d\\d\\d\\d")
 
   expect_error(SSNProvider$new("ex_EX"),
     "ex_EX not in set of available locales")
@@ -18,7 +18,7 @@ context("ch_ssn works")
 
 test_that("ch_ssn", {
   expect_is(ch_ssn(), "character")
-  expect_match(ch_ssn(), "\\d\\d\\d-\\d\\d-\\d\\d\\d\\d") # :(
+  expect_match(ch_ssn(), "\\d\\d\\d-\\d\\d-\\d\\d\\d\\d") 
   expect_equal(length(ch_ssn(3)), 3)
 })
 
